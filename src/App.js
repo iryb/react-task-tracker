17b9d30d3b265c37e0947652d04c8ea7bcd80ac4
Irina <iryna.burianovata@gmail.com>
2021-02-15 23:01:19 +0200

    Added task reminder

--- src/App.js
+++ src/App.js
@@ -9,16 +9,19 @@ function App() {
     [
         {
             id: 1,
-            title: 'Task 1'
+            title: 'Task 1',
+            reminder: true
         },
         {
             id: 2,
-            title: 'Task 2'
+            title: 'Task 2',
+            reminder: false
         },
         {
             id: 3,
-            title: 'Task 3'
-        }
+            title: 'Task 3',
+            reminder: false
+        } 
     ]
   );
 
@@ -26,10 +29,27 @@ function App() {
   const deleteTask = (id) => {
     setTask(tasks.filter( (task) => task.id !== id ))
   }
+
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    setTask(
+      tasks.map((task) => 
+        task.id === id ? 
+        { ...task, reminder: !task.reminder }
+        : task
+      )
+    )
+  }
+
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks'}
+      {tasks.length > 0 ? 
+      <Tasks tasks={tasks} 
+      onDelete={deleteTask}
+      onToggle={toggleReminder}
+      /> 
+      : 'No Tasks'}
     </div>
   );
 }
